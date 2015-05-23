49ff325c9f99484fe3a1140758f080ebe98c4ad5
ilyaberg <ilya.berezhnoy@gmail.com>
2015-05-23 22:41:56 +0300

    Functions added, desctipton and eamples added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions allow to work with matrices and get an inverse matrix
+## from cache
 
-## Write a short comment describing this function
+
+## This function creates a list of functions to work with a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse_m <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse_m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv_m) inverse_m <<- inv_m
+        getinv <- function() inverse_m #returns inverse matrix
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+        
 }
 
 
-## Write a short comment describing this function
+## This function inverses a matrix if this was not done earlier, otherwise it
+## returns inversed matrix from chache 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inverse_m <- x$getinv()
+        if(!is.null(inverse_m)) {
+                message("getting cached data")
+                return(inverse_m)
+        }
+        data <- x$get()
+        inverse_m <- solve(data, ...)
+        x$setinv(inverse_m)
+        inverse_m
+        
 }
+
+##Below are two examples of matrices that can be inversed.  You can uncomment
+##the lines and test the functions performance
+
+#m <- matrix(c(2, 6, 5, 5, 3, -2, 7, 4, -3), nrow = 3, ncol = 3)
+#m
+
+#nm <- matrix(c(3, 6, 5, 5, 3, -2, 7, 4, -3), nrow = 3, ncol = 3) #same as m, but first element is changed
+
+
+#z <- makeCacheMatrix(m)
+#z$get()
+
+#z$getinv() #returns NULL as inverse matrix has not been set yet
+
+#z$setinv(solve(m)) #here the matrix is set 
+
+#z$getinv() #retuns inverse matix
+
+#cacheSolve(z) #returns inverse matrix from cache
+
+#z$set(nm)
+#z$get()
+#z$getinv()
+#cacheSolve(z) #returns inverse matrix but not from cache
